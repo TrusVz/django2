1e1e1c3724bd31fbf628549696e093b383228af2
TrusVz <rondonluis982@gmail.com>
2024-09-21 12:40:26 -0400

    Retoques

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import PostList
 
 urlpatterns = [
-    path("", PostList.as_view(), name = "posts_list"),
+    path("", PostList.as_view(), name = "post_list"),
 ]
